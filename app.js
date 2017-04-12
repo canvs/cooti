717fa8cb8e3bb6d35deaa1be4c88de75f6c23e57
Zhengdan <476685852@qq.com>
2017-04-12 19:25:25 +0800

    fixstyle

--- app.js
+++ app.js
@@ -12,4 +12,4 @@ app.set('view engine', 'html');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use('/',indexRouter);
-app.listen(80);
+app.listen(3000);
--- router/index.js
+++ router/index.js
@@ -1,25 +1,39 @@
-const express   = require('express');
-const template  = require('art-template');
-const router    = express.Router();
+const express     = require('express');
+const template    = require('art-template');
+const mongodb     = require('mongodb');
+const mongoose    = require('mongoose');
+const mongoClient = mongodb.MongoClient;
+const app         = express();
+const router      = express.Router();
+
+
+// const DB_CONN_STR = 'mongodb://localhost:27017/cooti';
+
+const monthJX = ['JAN','FEB','MAR','APR','MAY','JUN','JUL','AUG','SEP','OCT','NOV','DEC'];
+const image   = ['star','people'];
+let timeTm    = new Date();
+let monthly   = monthJX[timeTm.getMonth()].split("");
+let random    = '/image/4.png' //parseInt(Math.floor(Math.random()*10)+1)+'.png';
 
 const data = {
-    title:"东隅的首页",
+    title:"东隅",
     time:(new Date).toString(),
+    monthly:monthly,
+    image:random,
     itemList:[{
         name:'请叫我英雄',
         id:0,
         descript:'我是英雄'
 
-    },{
-        name:'冻鱼',
-        id:1,
-        descript:'东隅已逝'
     }]
 }
+// mongoClient.connect(DB_CONN_STR,(err,db)=>{
 
+//     console.log('链接成功');
+// })
 router.get('/',(req,res)=>{
     
-    res.render('../views/main',data);
+    res.render('../views/home',data);
 })
 
 module.exports = router;
