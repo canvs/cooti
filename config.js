2396e7115da1f0f12028874d5189f4f11b591ab7
Zhengdan <476685852@qq.com>
2018-04-27 16:08:26 +0800

    new change

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
     port: 8888,
     session: {
-        secret: 'cooti.cn',
-        key:'cooti.cn',
+        secret: 'home.cooti.cn',
+        key:'home.cooti.cn',
         maxAge:2592000000
     },
     mongodb:'mongodb://localhost:27017/cooti'
